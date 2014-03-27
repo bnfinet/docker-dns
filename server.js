6825784c9c54210dfab54c9336ad5c0d918fc413
Benjamin Foote <git@bnf.net>
2014-03-27 18:58:38 +0000

    working multihost

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var srv = [];
 
 var main = function() {
 	initializeDockers(function() {
-		pollDockers(function(err, res) {
+		pollDockers(function(err) {
 			if (config.debug) {
 				console.log(a);
 				console.log(cname);
@@ -36,7 +36,8 @@ var main = function() {
 
 				// pollForEvents();
 				// setInterval(pollForEvents, pollInterval);
-				setInterval(refreshRecs, pollInterval);
+//				setInterval(refreshRecs, config.pollinterval);
+
 			}
 		});
 	});
@@ -51,21 +52,26 @@ var initializeDockers = function (cb) {
 
 var pollDockers = function(cb) {
 	async.each(config.dockers, function(docker, done) {
+	    if (config.debug) {
+		console.log("pollling docker " + docker.publicname);
+	    }
 		docker.D.listContainers({
 			all : 1
 		}, function(err, containers) {
+		    if (config.debug) {
+			console.log("inspecting " + containers.length + " containers");
+		    }
+
 			// TODO create a temp var to hold the recs while they get built
-			async.map(containers, function(container, next) {
+			async.each(containers, function(container, next) {
+			    if (config.debug) {
+				console.log("inspecting " + container.Id);
+			    }
+
+
 				inspectAndBuild(docker, container, next);
-			}, function(err, res) {
-				if (err) {
-					console.log(err);
-				} else {
-					cb(res);
-				}
-			});
+			}, done);
 		});
-		done();
 	}, cb);
 };
 
@@ -80,53 +86,69 @@ var refreshRecs = function() {
 };
 
 
-var buildRecords = function (insp, c, fqdnFn, ip, cb) {
+var buildRecords = function (docker, insp, c, fqdnFn, ip, cb) {
+    if (config.debug) {
+	console.log("building records for " + c.Id);
+    }
+
+
 	// A UUID -> ip
-	a[fqdnFn(c.Id)] = new named.ARecord(ip);
+	a[fqdnFn(docker, c.Id)] = new named.ARecord(ip);
 
 	// A first12(UUID) -> ip
 	var uuid12 = c.Id.substring(0, 12);
-	a[fqdnFn(uuid12)] = new named.ARecord(ip);
+	a[fqdnFn(docker, uuid12)] = new named.ARecord(ip);
 
 	// CNAME hostname -> first12(UUID)
 	if (insp.Config.Hostname) {
-		cname[fqdnFn(insp.Config.Hostname)] = new named.CNAMERecord(
-				fqdnFn(uuid12));
+	    if (config.debug) {
+		console.log("hostname: " + insp.Config.Hostname);
+	    }
+			
+		cname[fqdnFn(docker, insp.Config.Hostname)] = new named.CNAMERecord(
+				fqdnFn(docker, uuid12));
+
 	}
 	// container name, or clean name
 	var cName = cleanName(insp.Name);
 	if (cName) {
-		cname[fqdnFn(cName)] = new named.CNAMERecord(fqdnFn(uuid12));
+		cname[fqdnFn(docker, cName)] = new named.CNAMERecord(fqdnFn(docker, uuid12));
 	}
 	var iHPb = insp.HostConfig.PortBindings;
 	if (!iHPb) {
 		cb();
 	} else {
 		async.each(Object.keys(iHPb), function(portproto, done) {
-			// foreach portbinding
-			if (portproto && iHPb[portproto]) {
-				if (config.debug) {
-					console.log('ports: ', iHPb[portproto]);
-				}
-				var port = iHPb[portproto][0].HostPort;
-				putsrvrec(portproto, uuid12, uuid12, port);
-				// SRV record _service._proto.hostname.faketld port
-				// first12.faketld
-				if (insp.Config.Hostname) {
-					putsrvrec(portproto, insp.Config.Hostname, uuid12, port);
-					putsrvrecForServiceName(portproto, insp.Config.Hostname, uuid12, port);
-				}
-				if (cName) {
-					putsrvrec(portproto, cName ,uuid12, port);
-					putsrvrecForServiceName(portproto, cName ,uuid12, port);
-				}
+		    if (portproto && iHPb[portproto]) {
+			if (config.debug) {
+			    console.log('ports: ', iHPb[portproto]);
 			}
-			done();
+			var port = iHPb[portproto][0].HostPort;
+			putsrvrec(docker, portproto, uuid12, uuid12, port, fqdnFn);
+			// SRV record _service._proto.hostname.faketld port
+			// first12.faketld
+			if (insp.Config.Hostname) {
+			    putsrvrec(docker, portproto, insp.Config.Hostname, uuid12, port, fqdnFn);
+			    putsrvrecForServiceName(docker, portproto, insp.Config.Hostname, uuid12, port, fqdnFn);
+			}
+			if (cName) {
+			    putsrvrec(docker, portproto, cName ,uuid12, port, fqdnFn);
+			    putsrvrecForServiceName(docker, portproto, cName ,uuid12, port, fqdnFn);
+			}
+		    }
+		    done();
 		}, function() {
-			cb();
+			    if (config.debug) {
+				console.log("calling back from buildRecord");
+			    }
+
+		    cb();
 		});
 	}
 };
+
+
+
 
 var inspectAndBuild = function(docker, c, cb) {
 	// console.log(c);
@@ -135,13 +157,26 @@ var inspectAndBuild = function(docker, c, cb) {
 		if (err) {
 			console.log(err);
 		} else {
-			aynch.each(getips(docker, insp), function(ip, done) {
-				if (ip) {
-					buildRecords(insp, c, publicfqdn, ips.exposed, done);
-				} else {
-					done();
-				}
-			}, cb);
+		    var ip = getips(docker, insp);
+
+		    if (config.debug) {
+			console.log("ip ", ip);
+		    }
+
+
+		    if (ip.internal) {
+			buildRecords(docker, insp, c, localfqdn, ip.internal, function() {
+
+			});
+		    }
+		    
+		    if (ip.exposed) {
+			buildRecords(docker, insp, c, publicfqdn, ip.exposed, function() {
+
+			});
+		    } 
+		    cb();
+		    
 		}
 	});
 };
@@ -153,55 +188,55 @@ var cleanName = function(name) {
 
 // the public fqdn
 var publicfqdn = function(docker, host) {
-	return actualfqdn(host, docker.publicname);
+	return _actualfqdn(host, docker.publicname);
 };
 
 // usually resolving to an ip associated with docker0 172.17.42.0/16
 var localfqdn = function(docker, host) {
-	return actualfqdn(host, docker.localname);
+	return _actualfqdn(host, docker.localname);
 };
 
-var actualfqdn = function(host, extra) {
+var _actualfqdn = function(host, extra) {
 	var ret = host + '.'; 
 	if (extra) {
-		ret = extra + '.';
+		ret = ret + extra + '.';
 	}
 	ret =  ret + config.faketld;
 	return ret;
 };
 
-var putsrvrecForServiceName = function(portproto, name, uuid12, port) {
+var putsrvrecForServiceName = function(docker, portproto, name, uuid12, port, fqdnFn) {
 	var s = esl.getService(portproto);
 	// add a traditional host specific RFC compliant SRV record
 	if (s) {
 		var srvname = '_' + s.service + '._' + s.proto + '.' + name;
-		putsrvrec(portproto, srvname, uuid12, port);
+	    putsrvrec(docker, portproto, srvname, uuid12, port, fqdnFn);
 	}
 };
 
-var putsrvrec = function(portproto, name, uuid12, port) {
+var putsrvrec = function(docker, portproto, name, uuid12, port, fqdnFn) {
 	// and do a 'skydns' style service record generalized for the host
-	if (!srv[fqdn(name)]) {
-		srv[fqdn(name)] = [];
+	if (!srv[fqdnFn(docker, name)]) {
+		srv[fqdnFn(docker, name)] = [];
 	}
 	// but see if it exists already before we push
-	var newrec = new named.SRVRecord(fqdn(uuid12), parseInt(port));
-	async.some(srv[fqdn(name)], function(rec, done) {
-		if (rec === newrec) {
+	var newrec = new named.SRVRecord(fqdnFn(docker, uuid12), parseInt(port));
+	async.some(srv[fqdnFn(docker, name)], function(rec, done) {
+		if (rec.target === newrec.target && rec.port === newrec.port) {
 			done(true);
 		} else {
 			done(false);
 		}
 	}, function(res) {
 		if (!res) {
-			srv[fqdn(name)].push(newrec);
+			srv[fqdnFn(docker, name)].push(newrec);
 		}
 	});
 };
 
 
 var getips = function(docker, insp) {
-	var ips = {
+	var ip = {
 			internal: insp.NetworkSettings.IPAddress,
 			exposed: null
 	};
@@ -211,12 +246,12 @@ var getips = function(docker, insp) {
 				.keys(insp.HostConfig.PortBindings)[0]][0].HostIp;
 		// console.log('getip ip: ', ip);
 		if (hostboundip === '0.0.0.0') {
-			ips.exposed = docker.publicip;
+			ip.exposed = docker.publicip;
 		} else if (hostbound !== ips.internal){
-			ips.exposed = hostboundip;
+			ip.exposed = hostboundip;
 		}
 	} 
-	return ips;
+	return ip;
 };
 
 
@@ -296,3 +331,6 @@ server.on('clientError', function(error) {
 server.on('uncaughtException', function(error) {
 	console.log("there was an excepton: ", error.message);
 });
+
+main();
+setInterval(main, config.pollinterval);
