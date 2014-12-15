5f6573f0874aec8da27a27ad7d0d8a78aba2d204
Benjamin Foote <git@bnf.net>
2014-12-15 00:15:58 -0800

    no need to have a config to build

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-if [ ! -e ./config/config.js ]
-then
-    cat <<EOF
-
-  you should have a config file in place before you build and run your docker image
-
-  ${PWD}/config/config.js
-  
-  see
-  https://github.com/bnfinet/docker-dns/blob/master/config/config.js.example
-
-EOF
-
- exit;
-fi
-
 # docker rmi docker-dns
 # docker build --no-cache --rm -t docker-dns .
 docker build --rm -t bfoote/docker-dns .
